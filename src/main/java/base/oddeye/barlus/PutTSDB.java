5b291ff07f6fdc424f0c5564881e63c9a3b11a24
Vahan Av <webdev@ifcmarkets.com>
2016-08-09 16:17:32 +0400

    Sendtsdb to kafka

--- src/main/java/base/oddeye/barlus/PutTSDB.java
+++ src/main/java/base/oddeye/barlus/PutTSDB.java
@@ -5,12 +5,24 @@
  */
 package base.oddeye.barlus;
 
+import static base.oddeye.barlus.write.log;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.logging.Level;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import kafka.producer.KeyedMessage;
+import kafka.utils.Json;
+import scala.Function1;
+import scala.Option;
+import scala.collection.immutable.Map;
+import scala.runtime.AbstractFunction1;
+import scala.util.parsing.json.JSON;
 
 /**
  *
@@ -29,18 +41,82 @@ public class PutTSDB extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PutTSDB</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PutTSDB at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        Option msgObject = null;
+        try {
+
+            String Httpresponse = "";
+            String uid = request.getParameter("UUID");
+            String msg = uid;
+            String topic = AppConfiguration.getBrokerTopic();
+            KeyedMessage<String, String> data = new KeyedMessage<String, String>(topic, msg);
+            msg = "";
+            Map JsonMap = null;
+            if ((uid != null) & (uid != "")) {
+                int idx = Arrays.binarySearch(AppConfiguration.getUsers(), uid, Collections.reverseOrder());
+                if (idx > -1) {
+                    msg = request.getParameter("data");
+                } else {
+                    AppConfiguration.initUsers();
+                    idx = Arrays.binarySearch(AppConfiguration.getUsers(), uid, Collections.reverseOrder());
+                    if (idx > -1) {
+                        msg = request.getParameter("data");
+                    } else {
+                        Httpresponse = "UUID Not exist User count = " + AppConfiguration.getUsers().length;
+                    }
+                }
+            } else {
+                msg = "";
+                Httpresponse = "UUID is empty";
+            }
+            if (msg != "") {
+
+                Function1<String, Object> f = new AbstractFunction1<String, Object>() {
+                    public Object apply(String s) {
+                        try {
+                            return Integer.parseInt(s);
+                        } catch (NumberFormatException e) {
+                            return Double.parseDouble(s);
+                        }
+
+                    }
+                };
+
+                JSON.globalNumberParser_$eq(f);
+                msgObject = JSON.parseFull(msg);
+                if (!msgObject.isEmpty()) {
+                    Object maps = msgObject.productElement(0);
+                    if (maps instanceof Map) {
+                        JsonMap = (Map) maps;
+                        if (!JsonMap.get("metric").isEmpty() & !JsonMap.get("tags").isEmpty() & !JsonMap.get("timestamp").isEmpty()) {
+                            if (JsonMap.get("UUID").productElement(0).equals(uid)) {
+//                            msg = msg;
+                                topic = AppConfiguration.getBrokerTSDBTopic();
+                                data = new KeyedMessage<String, String>(topic, Json.encode(msgObject.productElement(0)));
+
+                                AppConfiguration.getProducer().send(data);
+                                Httpresponse = "Data Sended";
+                            } else {
+                                Httpresponse = "UUID Not valid";
+                            }
+                        } else {
+                            Httpresponse = "JSON Not valid";
+                        }
+                    } else {
+                        Httpresponse = "Data Not Mapped";
+                    }
+                } else {
+                    Httpresponse = "Data Not Json";
+                }
+            }
+
+            response.setContentType(
+                    "text/html;charset=UTF-8");
+            try (PrintWriter out = response.getWriter()) {                
+                out.println(Httpresponse + "\n\r");
+                out.println("Send message " + data);
+            }
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Exception: ", e);
         }
     }
 
